c9fcdda0092ee75c6f5a0e74d4b285e6e1110b22
adesola <ava6969@rit.edu>
2025-04-20 19:11:41 -0700

    fixed replace

--- src/methods/temporal.cpp
+++ src/methods/temporal.cpp
@@ -335,7 +335,7 @@ namespace epoch_frame
             auto new_type = timestamp(arrow::TimeUnit::NANO, timezone);
             return Array{std::make_shared<arrow::TimestampArray>(
                 new_type, m_data->length(), m_data->data()->buffers[0], m_data->data()->buffers[1],
-                m_data->offset())};
+                m_data.null_count(), m_data->offset())};
         }
         catch (const std::exception& e)
         {
