9b40f7152c00b695a402b77809e46ef280e2a09d
adesola <ava6969@rit.edu>
2025-04-26 16:14:22 -0700

    fixed S3Mnaager

--- src/common/s3_manager.h
+++ src/common/s3_manager.h
@@ -3,33 +3,27 @@
 //
 
 #pragma once
-#include <filesystem>
-#include <arrow/filesystem/s3fs.h>
 #include <arrow/filesystem/localfs.h>
+#include <arrow/filesystem/s3fs.h>
 
-namespace epoch_frame {
-// Global S3 manager class for handling initialization/finalization
-class S3Manager {
-private:
-     std::atomic<bool> s3_initialized_;
-     std::shared_ptr<arrow::fs::S3FileSystem> s3fs_;
+namespace epoch_frame
+{
+    // Global S3 manager class for handling initialization/finalization
+    class S3Manager
+    {
+      private:
+        std::shared_ptr<arrow::fs::S3FileSystem> s3fs_;
+        S3Manager();
 
-    // Check if initialized
-    bool is_initialized() const;
+      public:
+        static S3Manager& Instance();
 
-    S3Manager();
+        // Get S3 filesystem, initializing if necessary
+        arrow::Result<std::shared_ptr<arrow::fs::S3FileSystem>> get_filesystem();
 
-public:
-    static S3Manager& Instance();
-
-    ~S3Manager();
-    // Get S3 filesystem, initializing if necessary
-    arrow::Result<std::shared_ptr<arrow::fs::S3FileSystem>> get_filesystem();
-
-    // Initialize S3 if not already initialized
-    arrow::Status initialize();
-    // Manual finalization (should rarely be needed)
-    arrow::Status finalize();
-
-};
-}
+        // Initialize S3 if not already initialized
+        void initialize();
+        // Manual finalization (should rarely be needed)
+        void finalize();
+    };
+} // namespace epoch_frame
